949482445815824051e3ae76f38a4311ce4b70ab
Deepak Gidwani <deepakgidwani12356@gmail.com>
2022-10-26 19:02:38 +0530

    added index page with css

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse 
 
 # Create your views here.
+
+def starting_page(request):
+    return render(request,"blog/index.html")
+
+def posts(request):
+    return HttpResponse("posts")
+
+def post_details(request):
+    return HttpResponse("post-details")
