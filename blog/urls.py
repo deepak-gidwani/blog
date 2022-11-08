adba9926135cd1d36e8e24d2e1c962e450ff50a6
Deepak Gidwani <deepakgidwani12356@gmail.com>
2022-11-08 10:19:03 +0530

    all done till models

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("",views.starting_page,name="starting-page"),
     path("posts",views.posts,name="post-page"),
-    path("/posts/<slug:slug>",views.post_details, name="post-detail-page")
+    path("posts/<slug:slug>",views.post_details, name="post-detail-page")
 ]
