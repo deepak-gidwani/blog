4b84171202e6074dc881f1f7172bdfe65528c35c
Deepak Gidwani <deepakgidwani12356@gmail.com>
2022-12-07 21:25:10 +0530

    production changes

--- my_site/settings.py
+++ my_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z^g33m%+8wzu^g@0vz3w6$k$hzy7w9sr5uimm#b#&un=%cvto=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ "*" ]
 
 
 # Application definition
@@ -123,6 +123,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
