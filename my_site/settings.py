265d6a53d9697957e31ed0c71ae8d3f4ca5118e9
deepak <deepakgidwani08@gmail.com>
2024-04-14 18:13:46 +0530

    added .env

--- my_site/settings.py
+++ my_site/settings.py
@@ -18,9 +18,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+from dotenv import load_dotenv
+import os
+load_dotenv()  # take environment variables from .env.
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z^g33m%+8wzu^g@0vz3w6$k$hzy7w9sr5uimm#b#&un=%cvto='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
